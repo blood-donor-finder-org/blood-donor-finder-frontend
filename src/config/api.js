5dccbb8d48f122062dd74998247b7645c688585a
Vasantha Priyan <23suca26@tcarts.in>
2026-03-17 13:11:44 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://blood-donor-backend-api-hjbtcyesbzhdd7dk.southeastasia-01.azurewebsites.net';
 
 export default API_BASE_URL;
